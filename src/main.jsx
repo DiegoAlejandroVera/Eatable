12aef4a65a3adfb02b8c4e3ead82865ca1ebb12c
Diego Alejandro Vera Trujillo <diegoverat1796@outlook.com>
2023-11-09 21:41:51 -0500

    ADD: global styles with emotion-react

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { Global } from "@emotion/react";
+import { GlobalStyles } from "./styles/global.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <Global styles={GlobalStyles} />
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
