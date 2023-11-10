0175696f8bd620f5defc2f226b5520c755167b6c
Diego Alejandro Vera Trujillo <diegoverat1796@outlook.com>
2023-11-10 15:28:32 -0500

    ADD: create and update product routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
-import CreateForm from "./pages/CreateForm";
+import Details from "./pages/Details";
+import Create from "./pages/Create";
+import Update from "./pages/Update";
 
 function App() {
   return (
     <Routes>
       <Route index element={<Home />} />
       <Route path="/" element={<Home />} />
-      <Route path="/create" element={<CreateForm />} />
+      <Route path="/create" element={<Create />} />
+      <Route path="/details/:id" element={<Details />} />
+      <Route path="/update/:id" element={<Update />} />
     </Routes>
   );
 }
